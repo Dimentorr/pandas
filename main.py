3ee685624cb1c1eb4c089207536f7fc3bee0525f
Dimentorr <94733623+Dimentorr@users.noreply.github.com>
2023-11-26 17:12:03 +1000

    Создан топ по часам и нахождение лучшего по продажам дня недели

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ import asyncio
 from timeit import default_timer as timer
 from tqdm import tqdm
 
+import datetime
+
+
+all_weekdays = {0: 'Понедельник',
+                1: 'Вторник',
+                2: 'Среда',
+                3: 'Четверг',
+                4: 'Пятница',
+                5: 'Суббота',
+                6: 'Воскресенье'}
 
 # Необходимо передать название файла из папки csv
 def csv_read(name_csv, test=False):
@@ -18,34 +28,35 @@ def csv_write(df, name_csv):
     df.to_csv(f'csv/test_output/{name_csv}')
 
 
-def async_part(main_df, second_df, name_col_main, name_col_second, condition_top,
+async def async_part(main_df, second_df, name_col_main, condition_top, name_col_second='Ссылка',
                      products_from=False, from_df=pd.DataFrame(), city=False, city_df=pd.DataFrame(),
                      original_dict=dict()):
     dict_for_answer = dict()
     for l in second_df.index:
         # Нахождение всех index(id), соответствующим условию в основной таблице
-        tmp = main_df[name_col_main].isin([second_df[name_col_second][l]])
-        tmp = tmp.loc[pd.DataFrame(tmp)[name_col_main]]
-
+        condition_id = main_df[name_col_main].isin([second_df[name_col_second][l]])
+        condition_id = condition_id.loc[pd.DataFrame(condition_id)[name_col_main]].index
         # Нахождение в основной таблице всех строк с необходимыми index(id)
         # Нахождение всех данных о прадажах для определённого магазина/склада
-        tmp_csv = main_df.loc[main_df.index.isin(tmp.index)]
+        condition_data = main_df.loc[main_df.index.isin(condition_id)]
         if products_from:
-            tmp_csv = tmp_csv.loc[(tmp_csv['Филиал'].isin(from_df['Ссылка']))]
-        # if city or products_from:
-        #     sum_count = np.sum(tmp_csv[condition_top])
-        # else:
-        sum_count = np.sum(tmp_csv[condition_top])
+            condition_data = condition_data.loc[(condition_data['Филиал'].isin(from_df['Ссылка']))]
+        sum_count = np.sum(condition_data[condition_top])
         if products_from:
             dict_for_answer[second_df['Наименование'][l]] = sum_count
         else:
             # second_df - branches
             # main - sales
-            # к этому моменту в tmp_csv должны быть строки для отдельного склада/магазина
+            # к этому моменту в condition_data должны быть строки для отдельного склада/магазина
             if city:
                 # сортируем поля из branches
-                temp = second_df.loc[(second_df['Ссылка'].isin(tmp_csv['Филиал']))]
-                key = city_df.loc[(city_df['Ссылка'].isin(temp['Город']), ['Наименование'])].values[0][0]
+                # print(second_df['Ссылка'].isin(condition_data['Филиал']))
+                temp = second_df.loc[(second_df['Ссылка'].isin(condition_data['Филиал']))]
+                # print(city_df.loc[(city_df['Ссылка'].isin(temp['Город']), ['Наименование'])].values[0][0])
+                try:
+                    key = city_df.loc[(city_df['Ссылка'].isin(temp['Город']), ['Наименование'])].values[0][0]
+                except IndexError:
+                    continue
                 if key in dict_for_answer:
                     dict_for_answer[key] = dict_for_answer[key] + sum_count
                 else:
@@ -53,6 +64,7 @@ def async_part(main_df, second_df, name_col_main, name_col_second, condition_top
             else:
                 key = second_df['Наименование'][l]
                 dict_for_answer[key] = sum_count
+        main_df.drop(labels=condition_id, axis=0)
     original_dict.update(dict_for_answer)
 
 
@@ -73,19 +85,20 @@ def async_part(main_df, second_df, name_col_main, name_col_second, condition_top
 # --------------------------------------------------------------------|
 # Возвращает Dataframe Наименование : Сумма                           |
 # --------------------------------------------------------------------|
-async def find_top(main_df, second_df, name_col_main, name_col_second, condition_top,
+async def find_top(main_df, objects_to_sort, name_col_main, condition_top, name_col_linc_on_objects='Ссылка',
                    products_from=False, from_df=pd.DataFrame(), city=False, city_df=pd.DataFrame(),
                    len_top=10, for_last_msg=''):
     COUNT_SLICE = 100
     start_func = timer()
     dict_for_answer = dict()
-    remains = 1 if len(second_df) % COUNT_SLICE != 0 else 0
+    remains = 1 if len(objects_to_sort) % COUNT_SLICE != 0 else 0
     min_point = 0
     max_point = COUNT_SLICE
-    for j in tqdm(range((len(second_df) // COUNT_SLICE) + remains)):
-        async_part(main_df, second_df[min_point + (j * COUNT_SLICE):max_point + (j * COUNT_SLICE)],
-                   name_col_main, name_col_second, condition_top, products_from=products_from,
-                   from_df=from_df, city=city, city_df=city_df, original_dict=dict_for_answer)
+    for j in tqdm(range((len(objects_to_sort) // COUNT_SLICE) + remains)):
+        await asyncio.create_task(async_part(main_df,
+                                  objects_to_sort[min_point + (j * COUNT_SLICE):max_point + (j * COUNT_SLICE)],
+                                  name_col_main, condition_top, products_from=products_from,
+                                  from_df=from_df, city=city, city_df=city_df, original_dict=dict_for_answer))
     dict_for_answer.update(dict_for_answer)
 
     names, sumaries = list(), list()
@@ -107,6 +120,37 @@ async def find_top(main_df, second_df, name_col_main, name_col_second, condition
 {for_last_msg}
 ------------------------------------------------
 {pd.DataFrame.from_dict(result)}''')
+
+
+def week_day(x):
+    return datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S').weekday()
+
+
+def hour(x):
+    return datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S').time().hour
+
+
+async def find_top_time(sales_main_df):
+    sales_df = sales_main_df[['Период', 'Количество']]
+
+    data_hour = dict()
+    data_weekday = dict()
+    sales_df['weekday'] = sales_df['Период'].apply(week_day)
+    sales_df['Период'] = sales_df['Период'].apply(hour)
+
+    for h in range(sales_df['weekday'].min(), sales_df['weekday'].max() + 1):
+        sum_count_hour = np.sum(sales_df['weekday'].loc[(sales_df['weekday'] == h)])
+        data_hour[h] = sum_count_hour
+    for d in range(sales_df['weekday'].min(), sales_df['weekday'].max() + 1):
+        sum_count_weekday = np.sum(sales_df['weekday'].loc[(sales_df['weekday'] == d)])
+        data_weekday[d] = sum_count_weekday
+    for k in dict(sorted(data_weekday.items(), key=lambda item: item[1], reverse=True)):
+        print(f'''День недели с наибольшим количеством продаж - {all_weekdays[k]}''')
+        break
+
+    print(f'''Статистика часов по продажам:''')
+    for h in dict(sorted(data_hour.items(), key=lambda item: item[1], reverse=True)):
+        print(f'''{h} ч. - {data_hour[h]}''')
 
 
 print("Чтение вводных csv-файлов...")
@@ -135,41 +179,28 @@ branches_warehouse = branches_filter.loc[(
         branches_filter['warehouse'] != -1), ['Ссылка', 'Наименование']]
 branches_shop = branches_filter.loc[(
         branches_filter['warehouse'] == -1), ['Ссылка', 'Наименование']]
-branches_shop.to_csv('csv/test_output/shops.csv')
-branches_warehouse.to_csv('csv/test_output/warehouses.csv')
 end = timer()
 print(f'''Время выполнения: {end - start}
 ------------------------------------------------''')
 
-
-async def main():
-    await asyncio.create_task(find_top(main_df=sales, second_df=branches_shop, name_col_main='Филиал',
-                                       name_col_second='Ссылка', condition_top='Количество',
-                                       for_last_msg='Десять первых магазинов по количеству продаж'))
-
-    await asyncio.create_task(find_top(main_df=sales, second_df=branches_warehouse, name_col_main='Филиал',
-                                       name_col_second='Ссылка', condition_top='Количество',
-                                       for_last_msg='Десять первых складов по количеству продаж'))
-
-    await asyncio.create_task(find_top(main_df=sales, second_df=products, name_col_main='Номенклатура',
-                                       name_col_second='Ссылка', condition_top='Количество', products_from=True,
-                                       from_df=branches_warehouse,
-                                       for_last_msg='Десять самых продаваемых товаров по складам'))
-
-    await asyncio.create_task(find_top(main_df=sales, second_df=products, name_col_main='Номенклатура',
-                                       name_col_second='Ссылка', condition_top='Количество', products_from=True,
-                                       from_df=branches_shop,
-                                       for_last_msg='Десять самых продаваемых товаров по магазинам'))
-
-    await asyncio.create_task(find_top(main_df=sales, second_df=branches, name_col_main='Филиал',
-                                       name_col_second='Ссылка', condition_top='Количество', city=True, city_df=cities,
-                                       for_last_msg='Десять городов, в которых больше всего продавалось товаров'))
-
-
 print('Начало выполнения алгоритмов:')
 start = timer()
 
-asyncio.run(main())
+# asyncio.run(find_top(main_df=sales, objects_to_sort=branches_shop, name_col_main='Филиал', condition_top='Количество',
+#          for_last_msg='Десять первых магазинов по количеству продаж'))
+#
+# asyncio.run(find_top(main_df=sales, objects_to_sort=branches_warehouse, name_col_main='Филиал', condition_top='Количество',
+#          for_last_msg='Десять первых складов по количеству продаж'))
+#
+# asyncio.run(find_top(main_df=sales, objects_to_sort=products, name_col_main='Номенклатура', condition_top='Количество',
+#          products_from=True, from_df=branches_warehouse, for_last_msg='Десять самых продаваемых товаров по складам'))
+#
+# asyncio.run(find_top(main_df=sales, objects_to_sort=products, name_col_main='Номенклатура', condition_top='Количество',
+#          products_from=True, from_df=branches_shop, for_last_msg='Десять самых продаваемых товаров по магазинам'))
+#
+# asyncio.run(find_top(main_df=sales, objects_to_sort=branches, name_col_main='Филиал', condition_top='Количество',
+#          city=True, city_df=cities, for_last_msg='Десять городов, в которых больше всего продавалось товаров'))
+asyncio.run(find_top_time(sales))
 
 end = timer()
 print(f'''Общее время выполнения: {end - start}
